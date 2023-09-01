287385b3684dcfaec9bd4829ac1c6ff7d37f7ba9
4gordi <4gordi@gmail.com>
2023-09-01 07:31:01 +0300

    Update samba-init.sh

--- samba-init.sh
+++ samba-init.sh
@@ -7,29 +7,3 @@ then
     echo "exited to due to presence of USB storage"
     exit
 fi
-
-sudo cat <<'EOF' | sudo tee /etc/samba/smb.conf
-[global]
-server min protocol = NT1
-workgroup = WORKGROUP
-usershare allow guests = yes
-map to guest = bad user
-allow insecure wide links = yes
-[share]
-Comment = Pi shared folder
-Path = /share
-Browseable = yes
-Writeable = Yes
-only guest = no
-create mask = 0777
-directory mask = 0777
-Public = yes
-Guest ok = yes
-force user = pi
-follow symlinks = yes
-wide links = yes
-EOF
-
-#if you wish to create a samba user with password you can use the following:
-#sudo smbpasswd -a pi
-sudo /etc/init.d/smbd restart
