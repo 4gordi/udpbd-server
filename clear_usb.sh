57e40313b8643526d00d8079e3f7df48f974d2c5
4gordi <4gordi@gmail.com>
2023-09-01 13:52:22 +0300

    Update clear_usb.sh

--- clear_usb.sh
+++ clear_usb.sh
@@ -4,9 +4,9 @@
 USBDisk_Present=`sudo fdisk -l | grep /dev/sd`
 if [ -n "${USBDisk_Present}" ]
 then
-    echo "exited to due to presence of USB storage"
+    echo "Exited to due to presence of USB storage"
+    echo "${USBDisk_Present}"
     exit
 fi
 
 echo "UDPBD-server is DOWN"
-printf "UDPBD-server is DOWN"
