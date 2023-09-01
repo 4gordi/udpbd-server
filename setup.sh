8da7aa8adab1158683c712b28f3a0a303f044829
4gordi <4gordi@gmail.com>
2023-09-01 11:03:25 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,7 @@
 # psx-pi-smbshare setup script
 #
 # *What it does*
-# This script will install and configure an smb share at /share
-# It will also compile ps3netsrv from source to allow operability with PS3/Multiman
 # It also configures the pi ethernet port to act as dhcp server for connected devices and allows those connections to route through wifi on wlan0
-# Finally, XLink Kai is installed for online play.
 #
 # *More about the network configuration*
 # This configuration provides an ethernet connected PS2 or PS3 a low-latency connection to the smb share running on the raspberry pi
@@ -31,22 +28,22 @@ sudo apt-get -y upgrade
 # Ensure basic tools are present
 sudo apt-get -y install screen wget git curl coreutils
 
-# Install and configure Samba
-sudo apt-get install -y samba samba-common-bin
-wget https://raw.githubusercontent.com/4gordi/udpbd-server/main/clear_usb.sh -O /home/${USER}/clear_usb.sh
-chmod 755 /home/${USER}/clear_usb.sh
-sudo cp /home/${USER}/clear_usb.sh /usr/local/bin
-sudo mkdir -m 1777 /share
+# Compile script
+make -f /home/${USER}/udpbd-server/Makefile
+chmod +x /home/${USER}/udpbd-server/udpbd-server
+sudo cp /home/${USER}/udpbd-server/udpbd-server /usr/local/bin
+
+# Install and configure server
+chmod 755 /home/${USER}/udpbd-server/clear_usb.sh
+sudo cp /home/${USER}/udpbd-server/clear_usb.sh /usr/local/bin
 
 # Install wifi-to-eth route settings
 #sudo apt-get install -y dnsmasq
-#wget https://raw.githubusercontent.com/4gordi/psx-pi-smbshare/master/wifi-to-eth-route.sh -O /home/${USER}/wifi-to-eth-route.sh
-#chmod 755 /home/${USER}/wifi-to-eth-route.sh
+#chmod 755 /home/${USER}/udpbd-server/wifi-to-eth-route.sh
 
 # Install USB automount settings
-wget https://raw.githubusercontent.com/4gordi/udpbd-server/main/automount-usb.sh -O /home/${USER}/automount-usb.sh
-chmod 755 /home/${USER}/automount-usb.sh
-sudo /home/${USER}/automount-usb.sh
+chmod 755 /home/${USER}/udpbd-server/automount-usb.sh
+sudo /home/${USER}/udpbd-server/automount-usb.sh
 
 # Set samba-init + ps3netsrv, wifi-to-eth-route, setup-wifi-access-point, and XLink Kai to run on startup
 # { echo -e "@reboot sudo bash /usr/local/bin/samba-init.sh\n@reboot sudo bash /home/${USER}/wifi-to-eth-route.sh"; } | crontab -u pi -
@@ -54,7 +51,7 @@ sudo /home/${USER}/automount-usb.sh
 
 # Start services
 sudo /usr/local/bin/clear_usb.sh
-#sudo /home/${USER}/wifi-to-eth-route.sh
+#sudo /home/${USER}/udpbd-server/wifi-to-eth-route.sh
 
 # Not a bad idea to reboot
 sudo reboot
